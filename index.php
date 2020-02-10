e091d28e27d9d3c468910a84d09720052cbd5e89
Kerrie Low <klow4@mail.greenriver.edu>
2020-02-10 13:43:06 -0500

    Controller class demo. Added controllers for home and eggs views.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * Kerrie Low
- * 1.15.20
+ * 1.15.2020
  * Full Stack Software Development
  * http://www.klow.greenriverdev.com/328/chicken/
+ * Modified: 2.10.2020
+ * Controller Classes
  */
 
 // turn on error reporting
@@ -14,14 +16,23 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 
 // instantiate F3
-// :: means static method
 $f3 = Base::instance();
 
+// instantiate a controller object
+$controller = new ChickenController($f3);
+
 // define a default route
-// -> calls an instance method
 $f3->route('GET /', function() {
-    $view = new Template();
-    echo $view->render('views/all-about-chickens.php');
+    // Better to use when you need it multiple times
+//    global $controller;
+//    $controller->home();
+
+    // better to use when you only need it once
+    $GLOBALS['controller']->home();
+});
+
+$f3->route('GET /eggs', function() {
+    $GLOBALS['controller']->eggs();
 });
 
 // run f3
